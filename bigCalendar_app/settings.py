32287dc54856985838ba0cde26aa40b358a86a76
zckernel <zckernel@gmail.com>
2026-04-28 17:24:39 +0000

    fix: host

--- bigCalendar_app/settings.py
+++ bigCalendar_app/settings.py
@@ -26,7 +26,13 @@ SECRET_KEY = 'REMOVED_DJANGO_SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+    "zckernel.com",
+    "www.zckernel.com",
+]
 
 
 # Application definition
