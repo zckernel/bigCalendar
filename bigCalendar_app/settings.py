e32a612a1361185ad78a8b6a5a097cf59bf8f666
zckernel <zckernel@gmail.com>
2026-04-29 20:39:25 +0000

    Fix: security

--- bigCalendar_app/settings.py
+++ bigCalendar_app/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'REMOVED_DJANGO_SECRET_KEY'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     "localhost",
