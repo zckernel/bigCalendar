bce2c792d8392a81d8487a743382676a68356ea9
zckernel <zckernel@gmail.com>
2026-05-01 09:28:44 +0000

    feat: prod deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,13 @@ cd /opt/bigCalendar
 
 git pull
 
-docker compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml up -d --build
+
+docker compose -f docker-compose.prod.yml exec -T bigcalendar \
+  python manage.py migrate --noinput
 
 docker compose -f docker-compose.prod.yml exec -T bigcalendar \
   python manage.py collectstatic --noinput
+
+sudo nginx -t && sudo systemctl reload nginx
+
