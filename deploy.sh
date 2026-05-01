1e63d22797f74efe649d956a1dfc2fb44b804a96
zckernel <zckernel@gmail.com>
2026-05-01 09:59:41 +0000

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
-
 set -e
 
 cd /opt/bigCalendar
 
 git fetch origin
-
 git reset --hard origin/main
 
 mkdir -p /opt/bigCalendar/staticfiles
 
 docker compose -f docker-compose.prod.yml up -d --build
 
-docker compose -f docker-compose.prod.yml exec -T bigcalendar \
-
-  python manage.py migrate --noinput
-
-docker compose -f docker-compose.prod.yml exec -T bigcalendar \
-
-  python manage.py collectstatic --noinput
+docker compose -f docker-compose.prod.yml exec -T bigcalendar python manage.py migrate --noinput
+docker compose -f docker-compose.prod.yml exec -T bigcalendar python manage.py collectstatic --noinput
 
 sudo nginx -t
-
 sudo systemctl reload nginx
